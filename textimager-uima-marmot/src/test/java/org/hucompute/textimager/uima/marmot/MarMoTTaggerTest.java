df0ab217d560e94b97d6a5c70c903f9bbd22db73
hemati <hemati@em.uni-frankfurt.de>
2017-07-05 13:54:24 +0200

    model replaced

--- textimager-uima-marmot/src/test/java/org/hucompute/textimager/uima/marmot/MarMoTTaggerTest.java
+++ textimager-uima-marmot/src/test/java/org/hucompute/textimager/uima/marmot/MarMoTTaggerTest.java
@@ -32,8 +32,7 @@ public class MarMoTTaggerTest {
 		AggregateBuilder builder = new AggregateBuilder();
 		builder.add(createEngineDescription(
 				MarMoTTagger.class
-//				,
-//				MarMoTTagger.PARAM_MODEL_LOCATION,"src/test/resources/de.marmot",
+				,MarMoTTagger.PARAM_MODEL_LOCATION,"http://cistern.cis.lmu.de/marmot/models/CURRENT/spmrl/de.marmot"
 				,MarMoTTagger.PARAM_POS_MAPPING_LOCATION,"src/main/resources/org/hucompute/textimager/uima/marmot/lib/pos-de-pretrained.map"
 				));
 		SimplePipeline.runPipeline(cas,builder.createAggregate());
