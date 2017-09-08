b8b2e5aa76d4efc4328b0571d2549efb6719763b
alex <alex@HPC>
2017-09-08 10:26:32 +0200

    Changed dynamic python path parameter

--- textimager-uima-polyglot/src/main/java/org/hucompute/textimager/uima/polyglot/PolyglotMorphology.java
+++ textimager-uima-polyglot/src/main/java/org/hucompute/textimager/uima/polyglot/PolyglotMorphology.java
@@ -22,7 +22,7 @@ import morphemeAnnotation.type.MorphemeAnnotation;
 /**
 * PolyglotMorphology
 *
-* @date 09.08.2017
+* @date 08.09.2017
 *
 * @author Alexander Sang
 * @version 1.0
@@ -38,7 +38,7 @@ public class PolyglotMorphology  extends SegmenterBase {
 	/**
      * Load the PythonPATH
      */
-    public static final String PARAM_PYTHON_PATH = ComponentParameters.PARAM_INTERN_TAGS;
+    public static final String PARAM_PYTHON_PATH = "PythonPathPolyglot";
     @ConfigurationParameter(name = PARAM_PYTHON_PATH, mandatory = false)
     protected String PythonPATH;
     
