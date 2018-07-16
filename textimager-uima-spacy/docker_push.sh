f92cc08fae010b9dad49bd5907dbaa945dbde533
Daniel Baumartz <baumartz@stud.uni-frankfurt.de>
2018-07-16 15:31:13 +0200

    SpaCy Dependency Parser

--- textimager-uima-spacy/docker_push.sh
+++ textimager-uima-spacy/docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-IMAGE_VER=2
+IMAGE_VER=3
 bash ./docker_build.sh
 sudo docker tag textimager-spacy texttechnologylab/textimager-spacy:${IMAGE_VER}
 sudo docker push texttechnologylab/textimager-spacy:${IMAGE_VER}
