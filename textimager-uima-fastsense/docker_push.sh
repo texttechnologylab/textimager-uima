8951954877c1275fdc115801480cbfc392ba1afe
Daniel Baumartz <baumartz@stud.uni-frankfurt.de>
2018-11-19 13:55:39 +0100

    Moved fastSense EN to own repository

--- textimager-uima-fastsense/docker_push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-IMAGE_VER=1
-bash ./docker_build.sh
-sudo docker tag textimager-fastsense-en texttechnologylab/textimager-fastsense-en:${IMAGE_VER}
-sudo docker push texttechnologylab/textimager-fastsense-en:${IMAGE_VER}
--- textimager-uima-fastsense/start_disambig_rest.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 src/main/python/disambig_server/disambig_server.py --model src/main/python/best_model/ --worker_count 1
