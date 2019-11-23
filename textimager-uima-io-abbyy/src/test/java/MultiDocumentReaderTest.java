1e99a965c92d9312cfa097fe0742808e1c7c567c
Manuel Stoeckel <manuel.stoeckel@stud.uni-frankfurt.de>
2019-11-23 16:12:30 +0100

    WIP

--- textimager-uima-io-abbyy/src/test/java/MultiDocumentReaderTest.java
+++ textimager-uima-io-abbyy/src/test/java/MultiDocumentReaderTest.java
@@ -4,6 +4,7 @@ import org.apache.uima.collection.CollectionReader;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.biofid.deep_eos.DeepEosTagger;
 import org.junit.Test;
 import textimager.uima.io.abby.MultiDocumentReader;
 
@@ -17,16 +18,20 @@ public class MultiDocumentReaderTest {
 	
 	@Test
 	public void testReader() {
-		String path = Paths.get("src/test/resources/Biodiversity/").toAbsolutePath().toString();
+		String path = Paths.get("src/test/resources/Botanische_Zeitschriften/").toAbsolutePath().toString();
 		
 		try {
 			System.out.println(path);
 			CollectionReader reader = CollectionReaderFactory.createReader(MultiDocumentReader.class,
 					MultiDocumentReader.PARAM_SOURCE_LOCATION, path);
-			AnalysisEngineDescription engine1 = AnalysisEngineFactory.createEngineDescription(
+			AnalysisEngineDescription engine1 = AnalysisEngineFactory.createEngineDescription(DeepEosTagger.class,
+					DeepEosTagger.PARAM_MODEL_NAME, "biofid",
+					DeepEosTagger.PARAM_PYTHON_SRC, "../deep-eos-uima/target/python",
+					DeepEosTagger.PARAM_VERBOSE, true);
+			AnalysisEngineDescription engine2 =  AnalysisEngineFactory.createEngineDescription(
 					JCas2JSONWriter.class,
 					JCas2JSONWriter.PARAM_TARGET_LOCATION, "/tmp/MultiDocumentReaderTest.json");
-			SimplePipeline.runPipeline(reader, engine1);
+			SimplePipeline.runPipeline(reader, engine1, engine2);
 		} catch (UIMAException | IOException e) {
 			e.printStackTrace();
 		}
