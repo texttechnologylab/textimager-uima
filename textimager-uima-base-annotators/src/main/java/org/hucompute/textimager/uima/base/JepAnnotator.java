cbbc1301c51844457b86f1bb4695e0fff0c0f5fb
Manuel Stoeckel <manuel.stoeckel@stud.uni-frankfurt.de>
2020-02-18 16:01:00 +0100

    Created workaround for loading 'libpython*' files in JepAnnotator

--- textimager-uima-base-annotators/src/main/java/org/hucompute/textimager/uima/base/JepAnnotator.java
+++ textimager-uima-base-annotators/src/main/java/org/hucompute/textimager/uima/base/JepAnnotator.java
@@ -1,16 +1,18 @@
 package org.hucompute.textimager.uima.base;
 
+import jep.JepException;
+import jep.MainInterpreter;
+import jep.PyConfig;
+import jep.SharedInterpreter;
+import org.apache.commons.io.filefilter.RegexFileFilter;
 import org.apache.uima.UimaContext;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.resource.ResourceInitializationException;
 
-import jep.Jep;
-import jep.JepException;
-import jep.MainInterpreter;
-import jep.PyConfig;
-import jep.SharedInterpreter;
-import jep.SubInterpreter;
+import java.io.File;
+import java.io.FileFilter;
+import java.nio.file.Paths;
 
 public abstract class JepAnnotator extends JCasAnnotator_ImplBase {
 	/**
@@ -19,44 +21,50 @@ public abstract class JepAnnotator extends JCasAnnotator_ImplBase {
 	public static final String PARAM_PYTHON_HOME = "pythonHome";
 	@ConfigurationParameter(name = PARAM_PYTHON_HOME, mandatory = false)
 	protected String pythonHome;
-
+	
 	/**
 	 * The path to libjep lib
 	 */
 	public static final String PARAM_LIBJEP_PATH = "libjepPath";
 	@ConfigurationParameter(name = PARAM_LIBJEP_PATH, mandatory = false)
 	protected String libjepPath;
-
+	
 	protected SharedInterpreter interp;
-
+	
 	@Override
 	public void initialize(UimaContext aContext) throws ResourceInitializationException {
 		super.initialize(aContext);
-		System.out.println("init: " +this.getClass().getName());
-
+		System.out.println("init: " + this.getClass().getName());
+		
 		try {
+			
 			if (!pythonHome.isEmpty()) {
+				// Workaround for loading python library files
+				File libDir = Paths.get(pythonHome, "lib").toAbsolutePath().toFile();
+				FileFilter libpythonFilter = new RegexFileFilter("libpython*");
+				for (File file : libDir.listFiles(libpythonFilter)) {
+					System.load(file.getAbsolutePath());
+				}
+				
 				PyConfig config = new PyConfig();
 				config.setPythonHome(pythonHome);
-				try{
+				try {
 					MainInterpreter.setInitParams(config);
-				}
-				catch(JepException e){
-
+				} catch (JepException e) {
+				
 				}
 			}
-
-			if (libjepPath!= null && !libjepPath.isEmpty()) {
+			
+			if (libjepPath != null && !libjepPath.isEmpty()) {
 				MainInterpreter.setJepLibraryPath(libjepPath);
 			}
 			interp = new SharedInterpreter();
-		}
-		catch (Exception ex) {
+		} catch (Exception ex) {
 			ex.printStackTrace();
 			throw new ResourceInitializationException(ex);
 		}
 	}
-
+	
 	@Override
 	public void destroy() {
 		try {
@@ -64,7 +72,7 @@ public abstract class JepAnnotator extends JCasAnnotator_ImplBase {
 		} catch (JepException e) {
 			e.printStackTrace();
 		}
-
+		
 		super.destroy();
 	}
 }
