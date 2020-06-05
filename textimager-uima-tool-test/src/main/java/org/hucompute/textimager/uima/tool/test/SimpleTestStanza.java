6bf7e449e6e9387c40d5ae69c6f2e9e03c04fb90
Lucas3011 <lucashammer10@yahoo.de>
2020-06-05 18:23:06 +0200

    fixes implementation of stanza

---
+++ textimager-uima-tool-test/src/main/java/org/hucompute/textimager/uima/tool/test/SimpleTestStanza.java
@@ -0,0 +1,35 @@
+package org.hucompute.textimager.uima.tool.test;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.fit.factory.AggregateBuilder;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.jcas.JCas;
+import org.hucompute.textimager.uima.base.PublicInter;
+import org.hucompute.textimager.uima.stanza.StanzaTagger;
+import org.hucompute.textimager.uima.util.XmlFormatter;
+
+import jep.JepException;
+
+
+public class SimpleTestStanza {
+
+	public static void main(String[] args) throws UIMAException, JepException {
+
+		
+		JCas cas = JCasFactory.createText("Die Gefangenen erhängten sich in ihrer Zelle. Die Dorfbewohner erhängten den Viehdieb an einem Baum.","de");
+		
+		AggregateBuilder builder = new AggregateBuilder();
+		builder.add(createEngineDescription(StanzaTagger.class,StanzaTagger.PARAM_PYTHON_HOME,"C:\\Users\\Lucas3011\\AppData\\Local\\Programs\\Python\\Python36"));
+		
+		//aufsetzen des Interpreters
+		PublicInter.setUpInter("C:\\Users\\Lucas3011\\AppData\\Local\\Programs\\Python\\Python36");
+		
+		SimplePipeline.runPipeline(cas,builder.createAggregate());
+	
+		System.out.println(XmlFormatter.getPrettyString(cas.getCas()));
+	}
+
+}
