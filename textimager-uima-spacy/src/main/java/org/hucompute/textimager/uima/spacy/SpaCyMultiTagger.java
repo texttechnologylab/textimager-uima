0de2382fdf2715e3cb554b840cca0cf758fd3533
maxkrawietz <57331236+maxkrawietz@users.noreply.github.com>
2020-06-16 18:57:16 +0200

    added Multitagger

---
+++ textimager-uima-spacy/src/main/java/org/hucompute/textimager/uima/spacy/SpaCyMultiTagger.java
@@ -0,0 +1,45 @@
+package org.hucompute.textimager.uima.spacy;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.factory.AggregateBuilder;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+
+
+public class SpaCyMultiTagger extends JCasAnnotator_ImplBase{
+	
+	
+	public void initialize(UimaContext aContext) throws ResourceInitializationException {
+		super.initialize(aContext);
+		System.out.println("init: " + this.getClass().getName());
+	}
+	
+	
+	
+	public void process(JCas aJCas) throws AnalysisEngineProcessException{
+		
+		String pyhome = "";
+		try {
+			AggregateBuilder builder = new AggregateBuilder();
+			builder.add(createEngineDescription(SpaCyTokenizer.class,SpaCyTokenizer.PARAM_PYTHON_HOME,pyhome));
+			builder.add(createEngineDescription(SpaCyTagger.class,SpaCyTagger.PARAM_PYTHON_HOME,pyhome));
+			builder.add(createEngineDescription(SpaCyParser.class,SpaCyParser.PARAM_PYTHON_HOME,pyhome));
+			builder.add(createEngineDescription(SpaCyNER.class,SpaCyNER.PARAM_PYTHON_HOME,pyhome));
+			
+			SimplePipeline.runPipeline(aJCas,builder.createAggregate());
+		}
+		catch(UIMAException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+}
