3b3e959885e18ed06f67b6115e41eb62a9a53dcd
Maxim Konca <maxim.konca@posteo.de>
2021-02-16 09:34:05 +0100

    tests

--- textimager-uima-textscorer/src/main/java/org/hucompute/textimager/uima/textscorer/TextScorerBase.java
+++ textimager-uima-textscorer/src/main/java/org/hucompute/textimager/uima/textscorer/TextScorerBase.java
@@ -27,7 +27,7 @@ public abstract class TextScorerBase extends JepAnnotator {
 	public void initialize(UimaContext aContext) throws ResourceInitializationException {
 		super.initialize(aContext);
 
-		System.out.println("initializing spacy base class...");
+		System.out.println("initializing textscorer base class...");
 
 		// set defaults
 		// TODO schönerer Weg?
@@ -90,66 +90,5 @@ public abstract class TextScorerBase extends JepAnnotator {
 			}
 			System.out.println("-------------------- Init done --------------------");
 		
-	}
-
-	// Adds the "words" and "spaces" arrays for spaCy to the JSON object
-	protected void jsonAddWordsAndSpaces(JCas aJCas, HashMap<String, Object> json) {
-		ArrayList<String> jsonWords = new ArrayList<>();
-		ArrayList<Boolean> jsonSpaces = new ArrayList<>();
-
-		Token lastToken = null;
-		for (Token token : JCasUtil.select(aJCas, Token.class)) {
-			// Recreate spaCy Doc Text: Add "space" token if more than 1 space between words
-			if (lastToken != null) {
-				if (lastToken.getEnd() == token.getBegin()) {
-					// No space
-					jsonWords.add(token.getCoveredText());
-					jsonSpaces.add(false);
-				} else {
-					int num = token.getBegin() - lastToken.getEnd();
-					if (num > 1) {
-						// Add space to last word
-						jsonSpaces.add(true);
-						// Add "space" token with num-1 spaces
-						jsonWords.add(new String(new char[num-1]).replace("\0", " "));
-						// ... followed by no space and the next word
-						jsonSpaces.add(false);
-						jsonWords.add(token.getCoveredText());
-					} else {
-						jsonWords.add(token.getCoveredText());
-						jsonSpaces.add(true);
-					}
-				}
-			} else {
-				jsonWords.add(token.getCoveredText());
-			}
-
-			lastToken = token;
-		}
-
-		// Handle last token
-		if (lastToken != null) {
-			if (aJCas.getDocumentText().length() == lastToken.getEnd())	{
-				jsonSpaces.add(false);
-			} else {
-				int num = aJCas.getDocumentText().length() - lastToken.getEnd();
-				if (num > 1) {
-					jsonSpaces.add(true);
-					jsonWords.add(new String(new char[num-1]).replace("\0", " "));
-					jsonSpaces.add(false);
-				} else {
-					jsonSpaces.add(true);
-				}
-			}
-		}
-		json.put("words", jsonWords);
-		json.put("spaces", jsonSpaces);
-	}
-
-	protected HashMap<String, Object>  buildJSON(JCas aJCas) {
-		HashMap<String, Object> json = new HashMap<>();
-		json.put("lang", aJCas.getDocumentLanguage());
-		jsonAddWordsAndSpaces(aJCas, json);
-		return json;
 	}
 }
