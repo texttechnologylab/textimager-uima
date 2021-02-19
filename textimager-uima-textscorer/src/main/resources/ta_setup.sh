e366246811d35b0a37f36e48d0f4e5e6971d1cab
Maxim Konca <maxim.konca@posteo.de>
2021-02-19 19:51:40 +0100

    bug fixes

--- textimager-uima-textscorer/src/main/resources/ta_setup.sh
+++ textimager-uima-textscorer/src/main/resources/ta_setup.sh
@@ -17,10 +17,7 @@ conda activate "$ENV_NAME"
 # install models
 echo "installing spacy models..."
 
-MODELS_DOWNLOADED="/home/mx/dev/data/spacy/de_core_news_lg-3.0.0.tar.gz"
-if [ ! -e $MODELS_DOWNLOADED ]; then
-  MODELS_DOWNLOADED="/resources/nlp/models/spacy/de_core_news_sm-2.3.0.tar.gz"
-fi
+MODELS_DOWNLOADED="/resources/nlp/models/spacy/de_core_news_lg-3.0.0.tar.gz"
 if [ ! -e $MODELS_DOWNLOADED ]; then
 	echo "downloading models..."
 	python3 -m spacy download de_core_news_lg
@@ -28,19 +25,12 @@ if [ ! -e $MODELS_DOWNLOADED ]; then
 	python3 -m spacy download ja_core_news_lg
 else
 	echo "using predownloaded models"
-	pip3 install /home/mx/dev/data/spacy/de_core_news_lg-3.0.0.tar.gz
-	pip3 install /home/mx/dev/data/spacy/en_core_web_lg-3.0.0.tar.gz
-	pip3 install /home/mx/dev/data/spacy/ja_core_news_lg-3.0.0.tar.gz
+	pip3 install /resources/nlp/models/spacy/de_core_news_lg-3.0.0.tar.gz
+	pip3 install /resources/nlp/models/spacy/en_core_web_lg-3.0.0.tar.gz
+	pip3 install /resources/nlp/models/spacy/ja_core_news_lg-3.0.0.tar.gz
 fi
 
-TORCH="/home/mx/Gits/tools/pytorch/dist/*.whl"
-if [ ! -e $TORCH ]; then
-  echo "installing torch"
-  pip3 install torch
-else
-  echo "installing torch for amd"
-  pip3 install /home/mx/Gits/tools/pytorch/dist/*.whl
-fi
+pip3 install torch
 # prevents  jep.JepException: <class 'ImportError'>: Can't determine version for bottleneck
 echo "updating packages"
 conda update --all
