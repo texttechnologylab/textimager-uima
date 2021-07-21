1cbbb677aab0d9c760647bdc0f2e89e0d5ea7d2c
Daniel Baumartz <email@dbaumartz.de>
2021-07-21 17:46:49 +0200

    Stanza sentiment: Added document sentiment and "selection" comment

--- textimager-uima-stanza/src/main/java/org/hucompute/textimager/uima/stanza/StanzaSentiment.java
+++ textimager-uima-stanza/src/main/java/org/hucompute/textimager/uima/stanza/StanzaSentiment.java
@@ -9,8 +9,11 @@ import org.hucompute.textimager.uima.base.DockerRestAnnotator;
 import org.hucompute.textimager.uima.type.Sentiment;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.texttechnologylab.annotation.AnnotationComment;
 
 public class StanzaSentiment extends DockerRestAnnotator {
+    private static final String SENTENCE_TYPE = "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence";
+
     @Override
     protected String getDefaultDockerImage() {
         return "textimager-uima-service-stanza";
@@ -61,17 +64,47 @@ public class StanzaSentiment extends DockerRestAnnotator {
 
     @Override
     protected void updateCAS(JCas aJCas, JSONObject jsonResult) throws AnalysisEngineProcessException {
+        // Always sentence based, calculate for full document here
+        double fullDocSentiment = 0;
+        int sentenceCount = 0;
+
         if (jsonResult.has("sentences")) {
             for (Object sen : jsonResult.getJSONArray("sentences")) {
                 JSONObject sentence = (JSONObject) sen;
 
                 int begin = sentence.getJSONObject("sentence").getInt("begin");
                 int end = sentence.getJSONObject("sentence").getInt("end");
+                double sentimentValue = sentence.getDouble("sentiment");
+
+                fullDocSentiment += sentimentValue;
+                sentenceCount += 1;
 
                 Sentiment sentiment = new Sentiment(aJCas, begin, end);
-                sentiment.setSentiment(sentence.getDouble("sentiment"));
+                sentiment.setSentiment(sentimentValue);
                 sentiment.addToIndexes();
+
+                // Always sentence based
+                AnnotationComment comment = new AnnotationComment(aJCas);
+                comment.setReference(sentiment);
+                comment.setKey("selection");
+                comment.setValue(SENTENCE_TYPE);
+                comment.addToIndexes();
             }
+        }
+
+        // Add document sentiment
+        if (sentenceCount > 0) {
+            fullDocSentiment = fullDocSentiment / sentenceCount;
+
+            Sentiment sentiment = new Sentiment(aJCas, 0, aJCas.getDocumentText().length());
+            sentiment.setSentiment(fullDocSentiment);
+            sentiment.addToIndexes();
+
+            AnnotationComment comment = new AnnotationComment(aJCas);
+            comment.setReference(sentiment);
+            comment.setKey("selection");
+            comment.setValue(SENTENCE_TYPE);
+            comment.addToIndexes();
         }
     }
 }
