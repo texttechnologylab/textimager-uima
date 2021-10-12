730fd5d4d7baa91ca237c051ad5d4c99d5889f50
Daniel Baumartz <email@dbaumartz.de>
2021-10-12 10:51:06 +0200

    added simple ddc test

--- textimager-uima-fasttext/src/test/java/org/hucompute/textimager/fasttext/labelannotator/LabelAnnotatorSimpleTest.java
+++ textimager-uima-fasttext/src/test/java/org/hucompute/textimager/fasttext/labelannotator/LabelAnnotatorSimpleTest.java
@@ -6,6 +6,7 @@ import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
+import org.dkpro.core.languagetool.LanguageToolLemmatizer;
 import org.dkpro.core.languagetool.LanguageToolSegmenter;
 import org.hucompute.textimager.uima.type.category.CategoryCoveredTagged;
 import org.junit.Test;
@@ -14,6 +15,37 @@ import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDesc
 
 
 public class LabelAnnotatorSimpleTest {
+
+	@Test
+	public void testDdcDe() throws UIMAException{
+		JCas cas = JCasFactory.createText("Das ist einfach nur ein Test.");
+		cas.setDocumentLanguage("de");
+
+		AggregateBuilder builder = new AggregateBuilder();
+
+		builder.add(createEngineDescription(LanguageToolSegmenter.class));
+		builder.add(createEngineDescription(LanguageToolLemmatizer.class));
+
+		builder.add(createEngineDescription(LabelAnnotator.class,
+				LabelAnnotator.PARAM_FASTTEXT_LOCATION, "/resources/nlp/bin/categorization/fastText_original_for_ducc_annotators/fasttext",
+				LabelAnnotator.PARAM_LANGUAGE_MODELS_LABELS, "de,/resources/nlp/models/categorization/ddc/ddc_2018/wikipedia_de.v8.lemma.nopunct.pos.no_functionwords_gnd_ddc.v4.with_categories-lr0.2-lrUR150-minC5-dim100-ep10000-vec_vec_token_lemmapos.vec.epoch5000.bin,98",
+				LabelAnnotator.PARAM_FASTTEXT_K, 100,
+				LabelAnnotator.PARAM_CUTOFF, false,
+				LabelAnnotator.PARAM_SELECTION, "text",
+				LabelAnnotator.PARAM_TAGS, "ddc2",
+				LabelAnnotator.PARAM_USE_LEMMA, true,
+				LabelAnnotator.PARAM_ADD_POS, false,
+				LabelAnnotator.PARAM_POSMAP_LOCATION, "/resources/nlp/models/categorization/am_posmap.txt",
+				LabelAnnotator.PARAM_REMOVE_FUNCTIONWORDS, true,
+				LabelAnnotator.PARAM_REMOVE_PUNCT, true
+		));
+
+		SimplePipeline.runPipeline(cas, builder.createAggregate());
+
+		for (CategoryCoveredTagged category : JCasUtil.select(cas, CategoryCoveredTagged.class)) {
+			System.out.println(category.getScore() + " " + category.getValue() + " " + category.getCoveredText());
+		}
+	}
 
 //	@Test
 //	public void simpleExampleDE() throws UIMAException{
