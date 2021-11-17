2ef0ef0a50baf6aa05669566f155085a9886ae19
Daniel Baumartz <email@dbaumartz.de>
2021-11-17 15:31:40 +0100

    Update dependencies for spacy-ie to fix project build

--- textimager-uima-gervader/run_gervader_docker.sh
+++ textimager-uima-gervader/run_gervader_docker.sh
@@ -1,1 +1,1 @@
- docker run --name textimager-uima-service-gervader -p 8000:8000 textimager-uima-service-gervader:0.4
+ docker run --name textimager-uima-service-gervader -p 8000:8000 textimager-uima-service-gervader:0.5
