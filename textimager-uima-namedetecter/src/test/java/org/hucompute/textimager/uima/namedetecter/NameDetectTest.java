93525b6950d598b6ebbec4974f2c397e84361c16
Giuseppe Abrami <abrami@em.uni-frankfurt.de>
2022-03-22 15:20:05 +0100

    Adding Test-Files Merge branch 'master' of https://github.com/texttechnologylab/textimager-uima adding GNfinder # Conflicts: #	textimager-uima-gnfinder/src/main/java/org/hucompute/textimager/uima/gnfinder/Gnfinder.java #	textimager-uima-gnfinder/src/test/java/org/hucompute/textimager/uima/gnfinder/GNfinderTest.java #	textimager-uima-spacy-ie/src/test/java/org/hucompute/textimager/uima/spacy/SpaCyInformationExtractorTestSmallMultiFromFile.java

--- textimager-uima-namedetecter/src/test/java/org/hucompute/textimager/uima/namedetecter/NameDetectTest.java
+++ textimager-uima-namedetecter/src/test/java/org/hucompute/textimager/uima/namedetecter/NameDetectTest.java
@@ -10,20 +10,54 @@ import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.hucompute.textimager.uima.util.XmlFormatter;
 import org.junit.Test;
+import org.texttechnologylab.utilities.helper.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
 import static org.junit.Assert.assertArrayEquals;
 
 public class NameDetectTest {
     @Test
+    public void TimeTest() throws UIMAException, IOException {
+
+        String testFile = NameDetectTest.class.getClassLoader().getResource("1000.txt").getPath();
+//        String testFile = NameDetectTest.class.getClassLoader().getResource("City.txt").getPath();
+//        String testFile = NameDetectTest.class.getClassLoader().getResource("Small.txt").getPath();
+
+        JCas cas = JCasFactory.createText(FileUtils.getContentFromFile(new File(testFile)), "de");
+
+        AnalysisEngineDescription nameDetector = createEngineDescription(NameDetect.class,
+                NameDetect.PARAM_REST_ENDPOINT, "http://rawindra.hucompute.org:8102"
+        );
+//        AnalysisEngineDescription nameDetector = createEngineDescription(NameDetect.class,
+//                NameDetect.PARAM_DOCKER_HOST_PORT, 8000
+//        );
+
+        long lStart = System.currentTimeMillis();
+
+        SimplePipeline.runPipeline(cas, nameDetector);
+
+        long lEnd = System.currentTimeMillis();
+
+        System.out.println(XmlFormatter.getPrettyString(cas));
+
+        System.out.println((lEnd - lStart) / 60000);
+
+        System.out.println("End");
+    }
+
+
+    @Test
     public void NameTest() throws UIMAException {
 
         JCas cas = JCasFactory.createText("Germany location Angela");
 
         Token t1 = new Token(cas, 0, 7);
-		t1.addToIndexes();
-		Token t2 = new Token(cas, 8, 16);
-		t2.addToIndexes();
+        t1.addToIndexes();
+        Token t2 = new Token(cas, 8, 16);
+        t2.addToIndexes();
         Token t3 = new Token(cas, 17, 23);
         t3.addToIndexes();
 
@@ -33,12 +67,19 @@ public class NameDetectTest {
 //        AnalysisEngineDescription nameDetector = createEngineDescription(NameDetect.class,
 //                NameDetect.PARAM_DOCKER_HOST_PORT, 8000
 //        );
+
+        long lStart = System.currentTimeMillis();
+
         SimplePipeline.runPipeline(cas, nameDetector);
 
-        String[] ents = new String[] { "ORG", "LOC", "ORG", "LOC"};
+        long lEnd = System.currentTimeMillis();
+
+        String[] ents = new String[]{"ORG", "LOC", "ORG", "LOC"};
         String[] casEnts = JCasUtil.select(cas, NamedEntity.class).stream().map(NamedEntity::getValue).toArray(String[]::new);
 
         System.out.println(XmlFormatter.getPrettyString(cas));
+
+        System.out.println((lEnd - lStart) % 60000);
 
         assertArrayEquals(ents, casEnts);
     }
