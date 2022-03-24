783b75ea9cfdf38dd4a4baed63355941dca1686c
Manuel Stoeckel <manuel.stoeckel@em.uni-frankfurt.de>
2022-03-24 17:54:11 +0100

    feat!: Added support for multiple endpoints and batching
    - The JCasAnnotator's parameters have been changed!

--- textimager-uima-trans-nlp/src/main/java/SemanticRoleLabeling.java
+++ textimager-uima-trans-nlp/src/main/java/SemanticRoleLabeling.java
@@ -1,6 +1,6 @@
+import com.google.common.collect.Lists;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
-import org.apache.http.client.fluent.Content;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.entity.ContentType;
 import org.apache.uima.UimaContext;
@@ -16,111 +16,220 @@ import org.json.JSONObject;
 import org.texttechnologylab.annotation.semaf.isobase.Entity;
 import org.texttechnologylab.annotation.semaf.semafsr.SrLink;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class SemanticRoleLabeling extends JCasAnnotator_ImplBase {
 
-    public static final String PARAM_HOST = "host";
-    @ConfigurationParameter(name = PARAM_HOST, mandatory = false, defaultValue = "localhost")
-    protected String host;
-
-    public static final String PARAM_PORT = "port";
-    @ConfigurationParameter(name = PARAM_PORT, mandatory = false, defaultValue = "5087")
-    protected Integer port;
+    public static final String PARAM_ENDPOINTS = "endpoints";
+    @ConfigurationParameter(
+            name = PARAM_ENDPOINTS,
+            description = "An array of trans-nlp-api endpoints, like 'https://localhost:5087/srl'."
+    )
+    protected String[] endpoints;
+    private final ConcurrentLinkedQueue<String> endpointQueue = new ConcurrentLinkedQueue<>();
 
     public static final String PARAM_LANGUAGE = "language";
-    @ConfigurationParameter(name = PARAM_LANGUAGE, mandatory = false, defaultValue = "de")
+    @ConfigurationParameter(name = PARAM_LANGUAGE, mandatory = false, defaultValue = "de", description = "The source " +
+            "language. For currently supported languages, see the apidocs of the endpoints.")
     protected String language;
 
-    protected String endpoint;
+    public static final String PARAM_BATCH_SIZE = "batchSize";
+    @ConfigurationParameter(
+            name = PARAM_BATCH_SIZE,
+            defaultValue = "64",
+            description = "While larger batch sizes generally improve performance by a tiny fraction, " +
+                    "the improvement is negligible in comparison to the increased time that the APIs are blocked " +
+                    "by a request. Keeping this to a reasonable value is recommended, default is 64."
+    )
+    protected Integer batchSize;
+    private ExecutorService executorService;
 
     @Override
     public void initialize(UimaContext context) throws ResourceInitializationException {
         super.initialize(context);
-        endpoint = String.format("http://%s:%d/srl", host, port);
+        executorService = Executors.newFixedThreadPool(endpoints.length);
+        endpointQueue.addAll(List.of(endpoints));
     }
 
     @Override
     public void process(JCas aJCas) throws AnalysisEngineProcessException {
+        ArrayList<Sentence> sentences = new ArrayList<>(JCasUtil.select(aJCas, Sentence.class));
+        List<String> sentenceStrings = sentences.stream()
+                .map(Annotation::getCoveredText)
+                .map(String::strip)
+                .collect(Collectors.toList());
+
+        List<JSONObject> response = processWithEndpoints(sentenceStrings);
+
+        annotate(aJCas, sentences, response);
+    }
+
+    /**
+     * @param sentenceStrings The sentences to process.
+     * @return A list of JSONObjects, as returned by the API
+     * @throws AnalysisEngineProcessException
+     */
+    private List<JSONObject> processWithEndpoints(List<String> sentenceStrings) throws AnalysisEngineProcessException {
+        // Split the list into ⌈#sentences/#endpoints⌉ sized chunks (last chunk may be only partially filled)
+        List<List<String>> chunkedSentences = Lists.partition(sentenceStrings, batchSize);
+
+        // Create Callables for each sentence chunk, wrap them in a future and start them in their own thread
+        ArrayList<Future<String>> tasks = new ArrayList<>();
+        for (List<String> chunk : chunkedSentences) {
+            tasks.add(executorService.submit(new SRLCallable(chunk, this.language)));
+        }
+
+        // Collect all response strings, which are serialized JSON arrays, in a single list
+        ArrayList<String> responses = new ArrayList<>(tasks.size());
         try {
-            ArrayList<Sentence> sentences = new ArrayList<>( JCasUtil.select(aJCas, Sentence.class));
-            List<String> sentenceStrings = sentences.stream()
-                    .map(Annotation::getCoveredText)
-                    .map(String::strip)
-                    .collect(Collectors.toList());
+            for (Future<String> task : tasks) {
+                responses.add(task.get());
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            throw new AnalysisEngineProcessException(e);
+        }
 
-            JSONObject requestJSON = new JSONObject();
-            requestJSON.put("sentences", new JSONArray(sentenceStrings));
-            requestJSON.put("lang", this.language);
-            Content content = Request.Post(endpoint)
-                    .bodyString(requestJSON.toString(), ContentType.APPLICATION_JSON)
-                    .execute()
-                    .returnContent();
-            JSONArray responseJSON = new JSONArray(content.toString());
-
-            Map<Integer, Token> tokenBeginMap = JCasUtil.select(aJCas, Token.class).stream().collect(Collectors.toMap(
-                    Token::getBegin,
-                    Function.identity(),
-                    (a, b) -> a
-            ));
-
-            for (int sentIdx = 0; sentIdx < sentences.size(); sentIdx++) {
-                int sentenceOffset = sentences.get(sentIdx).getBegin();
-
-                JSONObject sentenceResults = responseJSON.getJSONObject(sentIdx);
-                JSONArray srlResults = sentenceResults.getJSONArray("srl");
-                for (int predIdx = 0; predIdx < srlResults.length(); predIdx++) {
-                    JSONObject predicateJSON = srlResults.getJSONObject(predIdx);
-
-                    JSONArray character_aligned_tags = predicateJSON.getJSONArray("character_aligned_tags");
-                    int predBegin = -1;
-                    for (int tagIdx = 0; tagIdx < character_aligned_tags.length(); tagIdx++) {
-                        JSONArray tagArray = character_aligned_tags.getJSONArray(tagIdx);
-                        int tagBegin = tagArray.getInt(0);
-                        String tagValue = tagArray.getString(2);
-
-                        if (tagValue.equals("V")) {
-                            predBegin = tagBegin;
-                            break;
-                        }
+        // Using streams: map the response strings to JSONArrays (of JSONObjects),
+        // then extract the contained JSONObjects and flatten the result into a single list of JSONObjects
+        return responses.stream()
+                .map(JSONArray::new)
+                .map(array -> {
+                    ArrayList<JSONObject> chunkResponse = new ArrayList<>();
+                    for (int i = 0; i < array.length(); i++) {
+                        chunkResponse.add(array.getJSONObject(i));
                     }
-                    if (predBegin < 0 || !tokenBeginMap.containsKey(predBegin + sentenceOffset)) {
+                    return chunkResponse;
+                })
+                .flatMap(List::stream)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    /**
+     * Process the API responses and create the corresponding annotations in the JCas.
+     *
+     * @param aJCas     The current JCas.
+     * @param sentences The list of all sentences in the current JCas.
+     * @param responses The list of JSONObjects in the response from the API.
+     */
+    private void annotate(JCas aJCas, ArrayList<Sentence> sentences, List<JSONObject> responses) {
+        Map<Integer, Token> tokenBeginMap = JCasUtil.select(aJCas, Token.class).stream().collect(Collectors.toMap(
+                Token::getBegin,
+                Function.identity(),
+                (a, b) -> a
+        ));
+
+        for (int sentIdx = 0; sentIdx < sentences.size(); sentIdx++) {
+            int sentenceOffset = sentences.get(sentIdx).getBegin();
+
+            JSONObject sentenceResults = responses.get(sentIdx);
+            JSONArray srlResults = sentenceResults.getJSONArray("srl");
+            for (int predIdx = 0; predIdx < srlResults.length(); predIdx++) {
+                JSONObject predicateJSON = srlResults.getJSONObject(predIdx);
+                JSONArray character_aligned_tags = predicateJSON.getJSONArray("character_aligned_tags");
+
+                // Find the starting index of the predicate
+                int predBegin = -1;
+                for (int tagIdx = 0; tagIdx < character_aligned_tags.length(); tagIdx++) {
+                    JSONArray tagArray = character_aligned_tags.getJSONArray(tagIdx);
+                    int tagBegin = tagArray.getInt(0);
+                    String tagValue = tagArray.getString(2);
+
+                    if (tagValue.equals("V")) {
+                        predBegin = tagBegin;
+                        break;
+                    }
+                }
+                // If we could not find a predicate or the predicate is not present in the token mapping,
+                // skip this result
+                if (predBegin < 0 || !tokenBeginMap.containsKey(predBegin + sentenceOffset)) {
+                    continue;
+                }
+
+                Token predicateToken = tokenBeginMap.get(predBegin + sentenceOffset);
+                Entity predicateEntity = new Entity(aJCas, predicateToken.getBegin(), predicateToken.getEnd());
+                aJCas.addFsToIndexes(predicateEntity);
+
+                // Transfer annotations using the character-aligned tags
+                for (int tagIdx = 0; tagIdx < character_aligned_tags.length(); tagIdx++) {
+                    JSONArray tagArray = character_aligned_tags.getJSONArray(tagIdx);
+                    int tagBegin = tagArray.getInt(0);
+                    int tagEnd = tagArray.getInt(1);
+                    String tagValue = tagArray.getString(2);
+
+                    if (tagValue.equals("V") || tagValue.equals("O")) {
                         continue;
                     }
 
-                    Token predicateToken = tokenBeginMap.get(predBegin + sentenceOffset);
-                    Entity predciateEntity = new Entity(aJCas, predicateToken.getBegin(), predicateToken.getEnd());
-                    aJCas.addFsToIndexes(predciateEntity);
+                    Entity tagEntity = new Entity(aJCas, tagBegin + sentenceOffset, tagEnd + sentenceOffset);
+                    aJCas.addFsToIndexes(tagEntity);
 
-                    for (int tagIdx = 0; tagIdx < character_aligned_tags.length(); tagIdx++) {
-                        JSONArray tagArray = character_aligned_tags.getJSONArray(tagIdx);
-                        int tagBegin = tagArray.getInt(0);
-                        int tagEnd = tagArray.getInt(1);
-                        String tagValue = tagArray.getString(2);
-
-                        if (tagValue.equals("V") || tagValue.equals("O")) {
-                            continue;
-                        }
-
-                        Entity tagEntity = new Entity(aJCas, tagBegin + sentenceOffset, tagEnd + sentenceOffset);
-                        aJCas.addFsToIndexes(tagEntity);
-
-                        SrLink srLink = new SrLink(aJCas);
-                        srLink.setFigure(predciateEntity);
-                        srLink.setGround(tagEntity);
-                        srLink.setRel_type(tagValue);
-                        aJCas.addFsToIndexes(srLink);
-                    }
-
+                    SrLink srLink = new SrLink(aJCas);
+                    srLink.setFigure(predicateEntity);
+                    srLink.setGround(tagEntity);
+                    srLink.setRel_type(tagValue);
+                    aJCas.addFsToIndexes(srLink);
                 }
             }
-        } catch (IOException e) {
-            throw new AnalysisEngineProcessException(e);
+        }
+    }
+
+    class SRLCallable implements Callable<String> {
+
+        private final JSONObject requestJSON;
+
+        public SRLCallable(List<String> sentences, String language) {
+            requestJSON = new JSONObject();
+            requestJSON.put("sentences", new JSONArray(sentences));
+            requestJSON.put("lang", language);
+        }
+
+        @Override
+        public String call() throws Exception {
+            String endpoint = pollEndpoint();
+            String responseString = Request.Post(endpoint)
+                    .bodyString(requestJSON.toString(), ContentType.APPLICATION_JSON)
+                    .execute()
+                    .returnContent()
+                    .toString();
+            freeEndpoint(endpoint);
+            return responseString;
+        }
+
+        /**
+         * Get a free endpoint from the endpoint queue. Thread will wait if necessary.
+         *
+         * @return The URL of the free endpoint in a thread-safe manner.
+         */
+        private String pollEndpoint() {
+            synchronized (endpointQueue) {
+                String endpoint = endpointQueue.poll();
+                while (endpoint == null) {
+                    try {
+                        endpointQueue.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    endpoint = endpointQueue.poll();
+                }
+                return endpoint;
+            }
+        }
+
+        /**
+         * Free the endpoint again, appending it to the endpoint queue in a thread-safe manner.
+         *
+         * @param endpoint The endpoint that has been used by this thread.
+         */
+        private void freeEndpoint(String endpoint) {
+            synchronized (endpointQueue) {
+                endpointQueue.offer(endpoint);
+                endpointQueue.notify();
+            }
         }
     }
 }
--- textimager-uima-trans-nlp/src/test/java/SemanticRoleLabelingTest.java
+++ textimager-uima-trans-nlp/src/test/java/SemanticRoleLabelingTest.java
@@ -1,3 +1,5 @@
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import org.apache.uima.UIMAException;
@@ -10,23 +12,67 @@ import org.apache.uima.jcas.JCas;
 import org.junit.Test;
 import org.texttechnologylab.annotation.semaf.semafsr.SrLink;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SemanticRoleLabelingTest {
+    final boolean DEBUG = false;
 
-    @Test
-    public void testKafka() throws UIMAException {
-        JCas jCas = JCasFactory.createText("Als Gregor Samsa eines Morgens aus unruhigen Träumen erwachte, fand er sich in seinem Bett zu einem ungeheuren Ungeziefer verwandelt. Über dem Atlantik befand sich ein barometrisches Minimum; es wanderte ostwärts, einem über Russland lagernden Maximum zu, und verriet noch nicht die Neigung, diesem nördlich auszuweichen.");
-        Sentence kafkaSatz = new Sentence(jCas, 0, 133);
-        jCas.addFsToIndexes(kafkaSatz);
-        Sentence barometrischerSatz = new Sentence(jCas, 134, 321);
-        jCas.addFsToIndexes(barometrischerSatz);
+    private JCas getTinyCas() throws UIMAException {
+        JCas jCas = getCasFromList(List.of(
+                "Als Gregor Samsa eines Morgens aus unruhigen Träumen erwachte, fand er sich in seinem Bett zu einem ungeheuren Ungeziefer verwandelt.",
+                "Über dem Atlantik befand sich ein barometrisches Minimum; es wanderte ostwärts, einem über Russland lagernden Maximum zu, und verriet noch nicht die Neigung, diesem nördlich auszuweichen."
+        ));
+        tokenize(jCas);
+        return jCas;
+    }
 
-        for (Sentence sentence : JCasUtil.select(jCas, Sentence.class)) {
-            System.out.printf("'%s'\n", sentence.getCoveredText());
+    private JCas getCoNLLCas() throws UIMAException {
+        try {
+            List<String> strings = Files.readLines(new File(
+                            "src/main/resources/CoNLL2009-ST-German-train-filtered-plain.txt"),
+                    Charsets.UTF_8
+            ).subList(0, 257);
+
+            JCas jCas = getCasFromList(strings);
+
+            if (DEBUG) {
+                for (Sentence sentence : JCasUtil.select(jCas, Sentence.class)) {
+                    System.out.printf("'%s'\n", sentence.getCoveredText());
+                }
+            }
+
+            tokenize(jCas);
+
+            return jCas;
+        } catch (IOException e) {
+            throw new UIMAException(e);
+        }
+    }
+
+    private JCas getCasFromList(List<String> strings) throws UIMAException {
+        int lastEnd = -1;
+        ArrayList<Integer> startIndices = new ArrayList<>();
+        ArrayList<Integer> endIndices = new ArrayList<>();
+        for (String string : strings) {
+            startIndices.add(lastEnd + 1);
+            endIndices.add(lastEnd + string.length());
+            lastEnd = lastEnd + string.length();
         }
 
+        JCas jCas = JCasFactory.createText(String.join(" ", strings));
+        for (int i = 0; i < startIndices.size(); i++) {
+            Sentence sentence = new Sentence(jCas, startIndices.get(i), endIndices.get(i));
+            jCas.addFsToIndexes(sentence);
+        }
+        return jCas;
+    }
+
+    private void tokenize(JCas jCas) {
         Pattern pattern = Pattern.compile("(?=\\p{Blank}|$|((?<= )[^\\P{Punct}\\.\\-]+|[^\\P{Punct}\\-]+(?= )|(?<=^)[\\p{Punct}\\p{Blank}]+|[\\p{Punct}\\p{Blank}]+(?=$)))");
         Matcher matcher = pattern.matcher(jCas.getDocumentText());
 
@@ -42,15 +88,50 @@ public class SemanticRoleLabelingTest {
             if (prevIndex > jCas.getDocumentText().length())
                 break;
         }
-        for (Token token : JCasUtil.select(jCas, Token.class)) {
-            System.out.printf("'%s'\n", token.getCoveredText());
+        if (DEBUG) {
+            for (Token token : JCasUtil.select(jCas, Token.class)) {
+                System.out.printf("'%s'\n", token.getCoveredText());
+            }
         }
+    }
 
-        AnalysisEngine engine = AnalysisEngineFactory.createEngine(SemanticRoleLabeling.class);
+    @Test
+    public void testSingle() throws UIMAException {
+        JCas jCas = getTinyCas();
 
-        SimplePipeline.runPipeline(jCas,
-                engine
+        AnalysisEngine engine = AnalysisEngineFactory.createEngine(
+                SemanticRoleLabeling.class,
+                SemanticRoleLabeling.PARAM_ENDPOINTS, new String[]{
+                        "http://localhost:5087/srl"
+                }
         );
+
+        SimplePipeline.runPipeline(jCas, engine);
+
+        for (SrLink srLink : JCasUtil.select(jCas, SrLink.class)) {
+            System.out.printf(
+                    "'%s' -[%s]-> '%s'\n",
+                    srLink.getFigure().getCoveredText(),
+                    srLink.getRel_type(),
+                    srLink.getGround().getCoveredText()
+            );
+            System.out.println(srLink.toString(2));
+        }
+    }
+
+    @Test
+    public void testMulti() throws UIMAException {
+        JCas jCas = getTinyCas();
+
+        AnalysisEngine engine = AnalysisEngineFactory.createEngine(
+                SemanticRoleLabeling.class,
+                SemanticRoleLabeling.PARAM_ENDPOINTS, new String[]{
+                        "http://localhost:5087/srl",
+                        "http://localhost:5088/srl"
+                }
+        );
+
+        SimplePipeline.runPipeline(jCas, engine);
 
         for (SrLink srLink : JCasUtil.select(jCas, SrLink.class)) {
             System.out.printf(
